8c37c45b13a4d7050ad56c48f26c15c7d73218b9
Kimserey <kimserey.lam@groomgy.com>
2018-06-02 20:31:06 +0800

    Modified Locust

--- OrleansMinio.Locust/loadtest.py
+++ OrleansMinio.Locust/loadtest.py
@@ -3,11 +3,10 @@ from locust import HttpLocust, TaskSet, task
 class MyTaskSet(TaskSet):
     @task
     def saveGrain(self):
-        print("executing my_task")
         self.client.post("/account/balance?b=20")
 
 class MyLocust(HttpLocust):
     task_set = MyTaskSet
-    min_wait = 5000
-    max_wait = 15000
+    min_wait = 1000
+    max_wait = 1000
     host = "http://localhost:5000"
